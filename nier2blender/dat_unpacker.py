2f6c89e69b2ab37fd3602019a76fbc62d9da9a0b
Sourcery AI <>
2022-03-14 16:29:00 +0000

    'Refactored by Sourcery'

--- nier2blender/dat_unpacker.py
+++ nier2blender/dat_unpacker.py
@@ -65,66 +65,65 @@ Extension: %s'''%
 	return index,Filename,FileOffset,Size,Extension
 
 def extract_file(fp, filename, FileOffset, Size, extract_dir):
-	create_dir(extract_dir)
-	fp.seek(FileOffset)
-	FileContent = fp.read(Size)
-	outfile = open(extract_dir + '/'+filename,'wb')
-	print("extracting file %s to %s/%s"%(filename,extract_dir,filename))
-	outfile.write(FileContent)
-	outfile.close()
-	if filename.find('wtp') > -1 :
-		wtp_fp = open(extract_dir + '/'+filename,"rb")
-		content = wtp_fp.read(Size)
-		dds_group = content.split(b'DDS ')
-		dds_group = dds_group[1:]
-		for i in range(len(dds_group)):
-			print("unpacking %s to %s/%s"%(filename,extract_dir ,filename.replace('.wtp','_%d.dds'%i)))
-			dds_fp = open(extract_dir + '/'+filename.replace('.wtp','_%d.dds'%i), "wb")
-			dds_fp.write(b'DDS ')
-			dds_fp.write(dds_group[i])
-			dds_fp.close()
-		wtp_fp.close()
-		#os.remove("%s/%s"%(extract_dir,filename))
-	print("done")
+    create_dir(extract_dir)
+    fp.seek(FileOffset)
+    FileContent = fp.read(Size)
+    with open(f'{extract_dir}/{filename}', 'wb') as outfile:
+        print(f"extracting file {filename} to {extract_dir}/{filename}")
+        outfile.write(FileContent)
+    if filename.find('wtp') > -1:
+        with open(f'{extract_dir}/{filename}', "rb") as wtp_fp:
+            content = wtp_fp.read(Size)
+            dds_group = content.split(b'DDS ')
+            dds_group = dds_group[1:]
+            for i in range(len(dds_group)):
+                print(
+                    f"unpacking {filename} to {extract_dir}/{filename.replace('.wtp', '_%d.dds'%i)}"
+                )
+
+                with open(f'{extract_dir}/' + filename.replace('.wtp','_%d.dds'%i), "wb") as dds_fp:
+                    dds_fp.write(b'DDS ')
+                    dds_fp.write(dds_group[i])
+    		#os.remove("%s/%s"%(extract_dir,filename))
+    print("done")
 
 def get_all_files(path):
 	pass
 
 def main(filename, extract_dir, ROOT_DIR):
-	fp = open(filename,"rb")
-	headers = read_header(fp)
-	if headers:
-		FileCount, FileTableOffset, ExtensionTableOffset,NameTableOffset,SizeTableOffset,UnknownOffset1C,Unknown20 = headers
-		for i in range(FileCount):
-			extract_dir_sub = ''
-			index,Filename,FileOffset,Size,Extension = get_fileinfo(fp, i, FileTableOffset,ExtensionTableOffset, NameTableOffset,SizeTableOffset)
-			if extract_dir != '':
-				extract_dir_sub = extract_dir + '\\' + filename.replace(ROOT_DIR ,'') 
-				extract_file(fp, Filename, FileOffset, Size, extract_dir_sub)
+    fp = open(filename,"rb")
+    if headers := read_header(fp):
+        FileCount, FileTableOffset, ExtensionTableOffset,NameTableOffset,SizeTableOffset,UnknownOffset1C,Unknown20 = headers
+        for i in range(FileCount):
+        	extract_dir_sub = ''
+        	index,Filename,FileOffset,Size,Extension = get_fileinfo(fp, i, FileTableOffset,ExtensionTableOffset, NameTableOffset,SizeTableOffset)
+        	if extract_dir != '':
+        		extract_dir_sub = extract_dir + '\\' + filename.replace(ROOT_DIR ,'') 
+        		extract_file(fp, Filename, FileOffset, Size, extract_dir_sub)
 
 
 if __name__ == '__main__':
-	extract_dir = ''
-	dirname = ''
-	useage = "\nUseage:\npython dat_unpacker.py your_dat_path your_extract_path"
-	useage1 = "\nUseage:\nblender --background --python dat_unpacker.py your_dat_path your_extract_path"
-	if len(sys.argv) < 3:
-		print(useage)
-		exit()
-	if len(sys.argv) > 2:
-		dir_name = sys.argv[1]
-		extract_dir = sys.argv[2]
-		print()
-		if os.path.split(sys.argv[0])[-1].lower().find("blender") >-1:
-			if len(sys.argv) < 6:
-				print(useage1)
-				exit()
-			dir_name = sys.argv[4]
-			extract_dir = sys.argv[5]
-		if not os.path.exists(extract_dir):
-			create_dir(extract_dir)
-	ROOT_DIR = dir_name
-	for dirpath,dirnames,filename in os.walk(dir_name):
-		for file in filename:
-			filename = "%s\%s"%(dirpath,file)
-			main(filename, extract_dir, ROOT_DIR)
+    extract_dir = ''
+    dirname = ''
+    useage = "\nUseage:\npython dat_unpacker.py your_dat_path your_extract_path"
+    useage1 = "\nUseage:\nblender --background --python dat_unpacker.py your_dat_path your_extract_path"
+    if len(sys.argv) < 3:
+    	print(useage)
+    	exit()
+    if len(sys.argv) > 2:
+    	dir_name = sys.argv[1]
+    	extract_dir = sys.argv[2]
+    	print()
+    	if os.path.split(sys.argv[0])[-1].lower().find("blender") >-1:
+    		if len(sys.argv) < 6:
+    			print(useage1)
+    			exit()
+    		dir_name = sys.argv[4]
+    		extract_dir = sys.argv[5]
+    	if not os.path.exists(extract_dir):
+    		create_dir(extract_dir)
+    ROOT_DIR = dir_name
+    for dirpath,dirnames,filename in os.walk(ROOT_DIR):
+        for file in filename:
+        	filename = "%s\%s"%(dirpath,file)
+        	main(filename, extract_dir, ROOT_DIR)
--- nier2blender/wta.py
+++ nier2blender/wta.py
@@ -18,7 +18,7 @@ class WTA(object):
 			self.wtaTextureSize = [0] * self.textureCount
 			self.wtaTextureIdentifier = [0] * self.textureCount
 			self.unknownArray1 = [0] * self.textureCount
-			self.unknownArray2 = [] 
+			self.unknownArray2 = []
 			for i in range(self.textureCount):
 				wta_fp.seek(self.textureOffsetArrayOffset + i * 4)
 				self.wtaTextureOffset[i] = to_int(wta_fp.read(4))
@@ -29,18 +29,17 @@ class WTA(object):
 				wta_fp.seek(self.unknownArrayOffset1 + i * 4)
 				self.unknownArray1[i] = "%08x"%to_int(wta_fp.read(4))
 			wta_fp.seek(self.unknownArrayOffset2 )
-			unknownval =  (wta_fp.read(4))
-			while unknownval:
+			while unknownval := (wta_fp.read(4)):
 				self.unknownArray2.append(to_int(unknownval))
-				unknownval =  (wta_fp.read(4))
 			self.pointer2 = hex(wta_fp.tell())	
 	def getTextureByIndex(self, texture_index, texture_fp):
 		texture_fp.seek(self.wtaTextureOffset[texture_index])
-		texture = texture_fp.read(self.wtaTextureSize[texture_index])
-		return texture
+		return texture_fp.read(self.wtaTextureSize[texture_index])
 
 	def getTextureByIdentifier(self, textureIdentifier, texture_fp):
-		for index in range(self.textureCount):
-			if self.wtaTextureIdentifier[index] == textureIdentifier:
-				return self.getTextureByIndex(index,texture_fp)
-		return False
+		return next(
+		    (self.getTextureByIndex(index, texture_fp)
+		     for index in range(self.textureCount)
+		     if self.wtaTextureIdentifier[index] == textureIdentifier),
+		    False,
+		)
